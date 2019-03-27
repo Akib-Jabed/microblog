389d5cffd06af6ecaa9292bc08e2d58a17a4ac80
Akib Jabed <a.jabed.bd@gmail.com>
2019-03-27 21:39:20 +0600

    add bootstrap functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -15,6 +16,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 login.login_view = 'login'
 

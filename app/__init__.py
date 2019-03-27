b784803ea877834cc1a03e25d6f600195b469372
Akib Jabed <a.jabed.bd@gmail.com>
2019-03-27 22:46:50 +0600

    fix date time functionalities

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_login import LoginManager
 from flask_mail import Mail
 import logging
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 login.login_view = 'login'
 

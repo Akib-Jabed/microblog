015f518713453dccd3d8dc6c9f1f21060489058e
Akib Jabed <a.jabed.bd@gmail.com>
2019-03-30 21:18:20 +0600

    provide a better structure

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
+from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
-from app import db, login, app
+from app import db, login
 from hashlib import md5
 from time import time
 import jwt
@@ -66,12 +67,12 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            u_id = jwt.decode(token, app.config['SECRET_KEY'],
+            u_id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return

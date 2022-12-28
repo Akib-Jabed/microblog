8eec5d1f4be34c7162fe0bea129213cd2a8dddd7
Akib Jabed <a.jabed.bd@gmail.com>
2022-12-29 01:23:11 +0600

    add user profile page with auto generated avatar

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
 from app import app, db
 from app.models import User
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
+from datetime import datetime
 
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+        
 
 @app.get("/")
 @app.get("/index")
@@ -21,6 +29,24 @@ def index():
         }
     ]
     return render_template("index.html", title="Home", posts=posts)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {
+            "author": user,
+            "body": "This is post 01"
+        },
+        {
+            "author": user,
+            "body": "This is post 02"
+        }
+    ]
+    return render_template("user.html", user=user, posts=posts)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -39,11 +65,13 @@ def login():
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -58,3 +86,19 @@ def register():
         flash("User Registration Successful!!")
         return redirect(url_for('login'))
     return render_template('register.html', title="Sign Up", form=form)
+
+
+@app.route('/edit_profile', methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Profile has been updated")
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title="Edit Profile", form=form)

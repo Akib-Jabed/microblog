26b8e1a004495a5f0aa45551fc2d22d39e76f2eb
Akib Jabed <a.jabed.bd@gmail.com>
2022-12-29 16:06:22 +0600

    add customize error page for production server

--- config.py
+++ config.py
@@ -8,3 +8,10 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'app.db')
+    
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = os.getenv('MAIL_PORT')
+    MAIL_USE_LTS = os.getenv('MAIL_USE_LTS')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    ADMINS = os.getenv('ADMINS')

061ab4303b339d0d04ee812c8d7ea874688ac3d7
Akib Jabed <a.jabed.bd@gmail.com>
2023-01-02 22:20:52 +0600

    add pagination

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'app.db')
     
+    POSTS_PER_PAGE = 3
+     
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = os.getenv('MAIL_PORT')
     MAIL_USE_LTS = os.getenv('MAIL_USE_LTS')

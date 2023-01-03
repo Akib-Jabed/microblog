c83f82e94103571e5d27379a1d9dabab785a850d
Akib Jabed <a.jabed.bd@gmail.com>
2023-01-03 21:42:24 +0600

    update UI by adding bootstrap design

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
@@ -13,6 +14,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 

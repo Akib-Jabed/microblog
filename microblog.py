9a54584090d4fee4b27504e2971b05c9f239ee18
Akib Jabed <a.jabed.bd@gmail.com>
2023-10-27 12:01:13 +0600

    cleanup

--- microblog.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-from app.models import User, Post
-
-# customize the shell context so that after running `flask shell` along with app(Flask Application Instance) the db(Sqlalchemy Instance) and both `User` and `Post` models automatically pre-imported
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
--- old/microblog.py
+++
@@ -1,9 +0,0 @@
-from app import create_app, db
-from app.models import User, Post, Task, Message, Notification
-
-app = create_app()
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'user': User, 'post': Post, 'Task': Task,
-            'Message': Message, 'Notification': Notification}

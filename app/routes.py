1eb0a44a0015560445bdb976acf66c14df36d655
Akib Jabed <a.jabed.bd@gmail.com>
2023-12-15 22:39:17 +0600

    add user profile page

--- app/routes.py
+++ app/routes.py
@@ -41,6 +41,7 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
@@ -58,3 +59,20 @@ def register():
         flash('Congratulations, you are now a registered user!!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username==username))
+    posts = [
+        {
+            'author': user, 
+            'body': 'Test post #1'
+        },
+        {
+            'author': user, 
+            'body': 'Test post #2'
+        }
+    ]
+    
+    return render_template('user.html', user=user, posts=posts)

fba54ca729b595aa259400e6d59f0b7cad90bc02
Akib Jabed <a.jabed.bd@gmail.com>
2024-01-25 20:44:11 +0600

    add email support for password reset

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import jwt
+from time import time
 from hashlib import md5
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -5,7 +7,7 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa 
 import sqlalchemy.orm as so
-from app import db, login
+from app import app, db, login
 
 @login.user_loader
 def load_user(id):
@@ -79,6 +81,17 @@ class User(UserMixin, db.Model):
                 .where(sa.or_(Follower.id == self.id, Author.id == self.id))
                 .group_by(Post)
                 .order_by(Post.timestamp.desc()))
+        
+    def get_reset_password_token(self, expires_in=60):
+        return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in}, app.config['SECRET_KEY'], algorithm='HS256')
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
     
 
 class Post(db.Model):
